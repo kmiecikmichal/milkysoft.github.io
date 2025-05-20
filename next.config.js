e6e6f9429480c1bf94157c89b9dc23e675162c8c
Michal Kmiecik <kmiecikmichal.mk@gmail.com>
2025-05-20 21:48:54 +0200

    Fix build configuration and CSS handling

--- next.config.js
+++ next.config.js
@@ -8,13 +8,6 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/milkysoft.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/milkysoft.github.io' : '',
   distDir: 'out',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig; 
