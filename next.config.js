ca8d2cb2ab58d5ff29f7d2c71d58bb36cec5e327
Michal Kmiecik <kmiecikmichal.mk@gmail.com>
2025-05-20 23:07:39 +0200

    Simplify Next.js configuration for static export

--- next.config.js
+++ next.config.js
@@ -4,22 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   basePath: '/milkysoft.github.io',
   assetPrefix: '/milkysoft.github.io/',
-  distDir: 'out',
-  // Remove middleware and other server-side features
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig; 
